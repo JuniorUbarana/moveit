a6cabd40198c9ebf625499128f65e6c1f8eeb52b
Junior Ubarana <elci.junior@gmail.com>
2021-02-24 20:51:54 -0300

    Features: Contexts created

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,24 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar()  {
+
+  const { currentExperience, experinceToNextLevel } = useContext(ChallengesContext);
+
+  const percentToNextLevel = Math.round((currentExperience * 100) / experinceToNextLevel);
+  console.log(percentToNextLevel);
+
   return (
     <header className={styles.experinceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{ width: '50%' }}></div>
-        <span className={styles.currentExperience} style={{ left: '50%'}}>
-          300 xp
+        <div style={{ width: `${percentToNextLevel}%` }} />
+        <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+          { currentExperience } xp
         </span>
       </div>
-      <span>600 xp</span>
+      <span>0 xp</span>
     </header>
   );
 }
